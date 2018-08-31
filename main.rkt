521fa906fab3a811557ec96be5c142069d0022f8
guizamberlam <guizamberlam@hotmail.com>
2018-08-31 17:30:09 -0300

    Leitura do arquivo e separação funcionais

--- main.rkt
+++ main.rkt
@@ -8,5 +8,30 @@
 
 (define empresas (csvfile->list "dados.csv"))
 
-(struct dados_acoes (nome date open high low close adj volume))
+;(struct dados_acoes (nome date open high low close adj volume))
+(struct dados_acoes (nome date) #:transparent )
+
+(define microsoft (list))
+
+(define petrobras (list))
+
+(define google (list))
+
+(define (separa string)
+  (string-split string ";" #:repeat? #t))
+
+(define (addend x lst)
+  (cond [(empty? lst) (list x)]
+        [(empty? (rest lst)) (list (first lst) x)]
+        [else
+         (cons (first lst) (addend x (rest lst)))]))
+
+(define teste (dados_acoes "Guilherme" "Zamberlam"))
+(define teste2 (dados_acoes "Eduardo" "Torrezan"))
+
+(separa (first (first empresas)))
+
+(define (tenta lst)
+  (cond [(empty? (rest lst) (first lst))]
+        [else (dados_acoes 
 
