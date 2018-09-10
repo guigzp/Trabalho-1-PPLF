07848e58640874c66fab6e5a440a3e9e01cab13f
guizamberlam <guizamberlam@hotmail.com>
2018-09-09 21:21:43 -0300

    Funções de calculo funcionais

--- main.rkt
+++ main.rkt
@@ -3,6 +3,8 @@
 (require csv-reading)
 (require rackunit)
 (require rackunit/text-ui)
+;(require plot)
+;(require racket/gui)
 
 ; Arquivo csv -> Lista de listas
 ; Função que recebe um arquivo .csv e retorna uma lista de listas separando por quebra de linha, sendo cada linha uma lista.
@@ -12,7 +14,7 @@
 ; String -> Lista
 ; Função que recebe uma String e devolve uma Lista com os elementos da String original separados por ";"
 (define (separa string)
-  (string-split string ";" #:repeat? #t))
+  (string-split string "," #:repeat? #t))
 
 ; Le o .csv para arquivo
 (define arquivo (ler_arquivo "dados.csv"))
@@ -31,10 +33,16 @@
 ; Define a estrutura dos dados das ações
 (struct dados_acoes (nome date close) #:transparent)
 
+; String -> String
+; Inverte o ano com o dia de uma string de data do formato xx/xx/xxxx
+(define (inverte string)
+  (define x (reverse(string-split string "/" #:repeat? #t)))
+  (string-append (first x) "/" (second x) "/" (third x)))
+
 ; Lista de strings -> dados_acoes
 ; Recebe uma lista de strings e devolve um dado_acoes com os valores das strings
 (define (constroi dados)
-  (dados_acoes (first dados) (second dados) (string->number (sixth dados))))
+  (dados_acoes (first dados) (inverte (second dados)) (string->number (sixth dados))))
 
 ; String, Lista de Listas de String -> Lista de Listas de dados_acoes
 ; Filtra a lista de todas as empresas pela string passada, retornando uma lista de listas com somente as empresas com o nome passado
@@ -44,13 +52,13 @@
         [else (filtra_empresas nome (rest lst))]))
 
 ;Estrutura para armazenar os dados do Google
-(define google (filtra_empresas "Google" empresas))
+(define google (filtra_empresas "Google" arquivo))
 
 ;Estrutura para armazenar os dados da Petrobras
-(define petrobras (filtra_empresas "Petrobras" empresas))
+(define petrobras (filtra_empresas "Petrobras" arquivo))
 
 ;Estrutura para armazenar os dados da Microsoft
-(define microsoft (filtra_empresas "Microsoft" empresas))
+(define microsoft (filtra_empresas "Microsoft" arquivo))
 
 ; Lista de dados_acoes -> Numero
 ; Devolve a soma da coluna de fechamento de uma lista de ações
@@ -81,12 +89,6 @@
 (define petrobras_desordenado (shuffle petrobras))
 (define microsoft_desordenado (shuffle microsoft))
 
-; String -> String
-; Inverte o ano com o dia de uma string de data do formato xx/xx/xxxx
-(define (inverte string)
-  (define x (reverse(string-split string "/" #:repeat? #t)))
-  (string-append (first x) "/" (second x) "/" (third x)))
-
 ; dados_acoes -> dados_acoes
 ; Recebe um dados_acoes e devolve o mesmo com os campos da data com o ano e o dia trocados
 (define (inverte_data_acao acao)
@@ -106,6 +108,8 @@
   (define x (inverte_acoes lst))
     (inverte_acoes (sort x string<? #:key dados_acoes-date)))
 
+;(define (ordena_data lst)
+;   (sort lst string<? #:key dados_acoes-date))
 
 ; Lista de dados_acoes, Numero -> Numero
 ; Devolve a soma dos Numero primeiro termos da lista
@@ -121,17 +125,93 @@
         [(empty? acao) empty]
         [else (cons ( / (soma_qtd acao dias) dias) (media_movel (rest acao) dias))]))
 
+; Lista de dados_acoes, Numero, Numero -> Lista de Numeros
+; Calcula Media Movel Exponencial
+(define (media_movel_exponencial lst n media_anterior)
+  (define k (/ 2 (+ n 1)))
+  (cond [(empty? lst) empty]
+        [else
+         (define media_exponencial (+ (* k ( - (dados_acoes-close (first lst)) media_anterior)) media_anterior))
+         ;(define media_exponencial (+ (* k (dados_acoes-close (first lst))) (*(- 1 k) media_anterior)))
+         (cons media_exponencial (media_movel_exponencial (rest lst) n media_exponencial))]))
+
+; Avança lista
+(define (avanca_lista acao qtd)
+  (cond [(= qtd 0) acao]
+        [else (avanca_lista (rest acao) (sub1 qtd))]))
+
+; Chama o calculo da media exponencial
+(define (media_exponencial acao n)
+  (cond [(> n (length acao)) "Valor Inválido, superior ao tamanho do período"]
+        [else
+  (define primeira_media (first (media_movel acao n)))
+  (cons primeira_media (media_movel_exponencial (avanca_lista acao n) n primeira_media))]))
+
+; Diferença entre duas listas 
+(define (lista1_menos_lista2 lst1 lst2)
+  (cond [(empty? lst1) empty]
+        [(empty? lst2) empty]
+        [else (cons (- (first lst1) (first lst2)) (lista1_menos_lista2 (rest lst1) (rest lst2)))]))
+
+; MACD = Media exponencial de 12 dias - Media exponencial de 26 dias
+(define (macd acao)
+  (lista1_menos_lista2 (media_exponencial acao 12) (media_exponencial acao 26)))
+
+
+; Primeira Soma Média
+(define (primeira_soma_media tipo lista quantidade)
+  (cond [(= 0 quantidade) 0]
+        [(tipo (dados_acoes-close (second lista)) (dados_acoes-close (first lista)))
+               (define valor (abs (- (dados_acoes-close (second lista)) (dados_acoes-close (first lista)))))
+               (+ valor (primeira_soma_media tipo (rest lista) (sub1 quantidade)))]
+        [else (primeira_soma_media tipo (rest lista) (sub1 quantidade))]))
+
+; Primeira Perda Média
+(define (primeira_perda_media lista quantidade)
+  (/ (primeira_soma_media < lista (sub1 quantidade)) quantidade))
+
+; Primeiro Ganho Médio
+(define (primeiro_ganho_medio lista quantidade)
+  (/ (primeira_soma_media > lista (sub1 quantidade)) quantidade))
+
+
+; Força Relativa
+(define (forca_relativa acao quantidade)
+  (/ (primeiro_ganho_medio acao quantidade) (primeira_perda_media acao quantidade)))
+
+; RSI
+(define (calculo_rsi acao original quantidade)
+  (cond [(empty? acao) empty]
+        [else
+         (define valor (- 100 (/ 100(+ 1 (forca_relativa original quantidade)))))
+         (cons valor (calculo_rsi (rest acao) (rest original) quantidade))]))
+
+(define (rsi acao quantidade)
+  (calculo_rsi (avanca_lista acao quantidade) acao quantidade))
+
+(define x (list 44.34 44.09 44.15 43.61 44.33 44.83 45.10 45.42 45.84 46.08 45.89 46.03 45.61 46.28 46.28 46.00 46.03 46.41 46.22 45.64 46.21 46.25
+                45.71 46.45 45.78 45.35 44.03 44.18 44.22 44.57 43.42 42.66 43.13))
+
+(define y (list 6.02 5.95 5.95 5.98 5.83 5.81 5.86 6.06 6.09))
+                              
 ; String, dados_acao -> String
 ; Devolve a proxima data válida
-(define (proxima_data acao data)
-  (cond [(empty? acao) "Data não pertencente ao banco de dados"]
-        [(equal? (dados_acoes-date (first acao)) data)
-         (cond [(empty? (rest acao)) "Não possui data próxima válida"]
+(define (proxima_data acao data opcao)
+  (cond [(empty? acao) "Não existe data válida no banco de dados."]
+        [(opcao data (dados_acoes-date (first acao))) (dados_acoes-date (first acao))]
+        [(string=? data (dados_acoes-date (first acao)))
+         (cond [(empty? (rest acao)) "Não existe data válida no banco de dados."]
                [else (dados_acoes-date (first (rest acao)))])]
-        [else (proxima_data (rest acao) data)]))
+        [else (proxima_data (rest acao ) data opcao)]))
+
+; Chama a proxima_data
+(define (proxima_data_valida acao data)
+  (inverte (proxima_data (inverte_acoes acao) (inverte data)  string<?) ))
+
+; Chama a anterior data
+(define (anterior_data_valida acao data)
+  (inverte (proxima_data (reverse (inverte_acoes acao)) (inverte data)  string>?) ))
         
-
-
 (define ordenacao-tests
   (test-suite "Testes Ordenacao"
               (check-equal? (ordena_data google_desordenado) google)
@@ -140,10 +220,19 @@
 
 (define correlacao-tests
   (test-suite "Testes Correlacao"
-              (check-equal? (correlacao google microsoft) 0.16036975115981003)
-              (check-equal? (correlacao google petrobras) -0.07983751056182631)
-              (check-equal? (correlacao microsoft petrobras) 0.6372067611547209)))
+              (check-equal? (correlacao google microsoft) 0.1603697511597682)
+              (check-equal? (correlacao google petrobras) -0.07983751056172986)
+              (check-equal? (correlacao microsoft petrobras) 0.6372067611546479)))
 
 (define (executa-testes . testes)
 (run-tests (test-suite "Todos os testes" testes))
 (void))
+
+(define ys (macd google ))
+(define (gera teste)
+  (cond [(= teste 0) empty]
+        [else (cons teste (gera (sub1 teste)))]))
+(define xs (reverse (gera (length ys))))
+
+;(plot (discrete-histogram (map vector xs ys) #:color 'red #:gap 0.5) #:width 2000 #:y-max 15)
+;(plot (lines (map vector xs ys)) #:out-file "teste.png" #:out-kind 'png)
